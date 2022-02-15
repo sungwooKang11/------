565c011b9efb4c37f90cece3de488ebf2de14728
sungwookang <sungwookang11@gmail.com>
2022-02-15 16:39:16 +0900

    test

--- hidden.js
+++ hidden.js
@@ -2,7 +2,7 @@ const answerOpen = document.querySelector(".answerOpen");
 const answerClose = document.querySelector(".answerClose");
 const a = document.querySelector(".a");
 
-const openanswer = () => {
+const openanswer = () => { //삼항연산자 적용하기
     a.classList.remove("hidden");
     answerOpen.classList.add("hidden");
     answerClose.classList.remove("hidden");
